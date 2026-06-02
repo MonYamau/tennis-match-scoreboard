a1c3b2edaf4644d2875c4c04881702427d6a65ca
MonYamau <White.Fox1825@yandex.ru>
2026-06-02 17:57:39 +0500

    feat: correct Listener, configure at web.xml them;

--- src/main/java/com/project/listener/AppContextListener.java
+++ src/main/java/com/project/listener/AppContextListener.java
@@ -1,22 +1,47 @@
 package com.project.listener;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.dao.HibernateMatchDao;
 import com.project.dao.HibernatePlayerDao;
-import com.project.util.HibernateUtil;
+import com.project.dao.MatchDao;
+import com.project.dao.PlayerDao;
+import com.project.service.MatchRegistrationService;
+import com.project.storage.MatchStorage;
+import com.project.storage.RedisMatchStorage;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import redis.clients.jedis.RedisClient;
 
 public class AppContextListener implements ServletContextListener {
+    private SessionFactory sessionFactory;
+    private RedisClient redisClient;
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        HibernatePlayerDao playerDao = new HibernatePlayerDao(sessionFactory);
-        HibernateMatchDao matchDao = new HibernateMatchDao(sessionFactory);
+        ObjectMapper objectMapper = new ObjectMapper();
+        sessionFactory = new Configuration().configure().buildSessionFactory();
+        redisClient = RedisClient.builder().hostAndPort("localhost", 6379).build();
+
+        PlayerDao playerDao = new HibernatePlayerDao(sessionFactory);
+        MatchDao matchDao = new HibernateMatchDao(sessionFactory);
+        MatchStorage matchStorage = new RedisMatchStorage(objectMapper, redisClient);
+
+        MatchRegistrationService registrationService = new MatchRegistrationService(matchStorage, playerDao);
+
+        ServletContext context = sce.getServletContext();
+        context.setAttribute("RegistrationService", registrationService);
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        HibernateUtil.closeSessionFactory();
+        if (sessionFactory != null) {
+            sessionFactory.close();
+        }
+        if (redisClient != null) {
+            redisClient.close();
+        }
     }
 }
