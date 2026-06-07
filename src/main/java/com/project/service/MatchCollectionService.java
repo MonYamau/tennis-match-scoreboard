df0bb7a4a85ca436481b84ccd56af43b40bdc8a3
MonYamau <White.Fox1825@yandex.ru>
2026-06-07 19:37:07 +0500

    refactor: add constant and validation;

--- src/main/java/com/project/service/MatchCollectionService.java
+++ src/main/java/com/project/service/MatchCollectionService.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 
 public class MatchCollectionService {
     private final static int LIMIT_VALUE = 8;
+    private final static int UNIT_PAGE = 1;
 
     private final MatchMapper mapper = MatchMapper.INSTANCE;
     private final MatchDao matchDao;
@@ -20,22 +21,23 @@ public class MatchCollectionService {
     }
 
     public MatchPageDto findMatchesByFilters(int page, String namePattern) {
-        int offsetValue = (page - 1) * LIMIT_VALUE;
         boolean isEmptyFilter = namePattern == null || namePattern.isBlank();
         int pageCount = isEmptyFilter ? countPages() : countPagesWithFilter(namePattern);
+        int correctPage = Math.min(page, pageCount);
+        int offsetValue = (correctPage - 1) * LIMIT_VALUE;
         List<Match> matches = isEmptyFilter
                 ? matchDao.findPage(offsetValue, LIMIT_VALUE)
                 : matchDao.findPageByFilters(namePattern, offsetValue, LIMIT_VALUE);
         List<MatchDto> matchesDto = matches.stream()
                 .map(mapper::toDto)
                 .collect(Collectors.toList());
-        return new MatchPageDto(page, pageCount, namePattern, matchesDto);
+        return new MatchPageDto(correctPage, pageCount, namePattern, matchesDto);
     }
 
     private int countPages() {
         long countMatch = matchDao.countAll();
         if (countMatch % LIMIT_VALUE != 0) {
-            return (int) ((countMatch / LIMIT_VALUE) + 1);
+            return (int) ((countMatch / LIMIT_VALUE) + UNIT_PAGE);
         }
         return (int) (countMatch / LIMIT_VALUE);
     }
@@ -43,7 +45,7 @@ public class MatchCollectionService {
     private int countPagesWithFilter(String namePattern) {
         long countMatch = matchDao.countAllByFilter(namePattern);
         if (countMatch % LIMIT_VALUE != 0) {
-            return (int) ((countMatch / LIMIT_VALUE) + 1);
+            return (int) ((countMatch / LIMIT_VALUE) + UNIT_PAGE);
         }
         return (int) (countMatch / LIMIT_VALUE);
     }
