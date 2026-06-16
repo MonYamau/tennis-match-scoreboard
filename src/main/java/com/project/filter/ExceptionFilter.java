69c5f39e342c5d16826d6360fa6a814da89e464a
MonYamau <White.Fox1825@yandex.ru>
2026-06-16 16:05:57 +0500

    feat: add logback;

--- src/main/java/com/project/filter/ExceptionFilter.java
+++ src/main/java/com/project/filter/ExceptionFilter.java
@@ -6,10 +6,14 @@ import com.project.util.JspPages;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class ExceptionFilter implements Filter {
+    private static final Logger log = LoggerFactory.getLogger(ExceptionFilter.class);
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
@@ -21,10 +25,11 @@ public class ExceptionFilter implements Filter {
         } catch (DataNotFoundException e) {
             handleException(req, resp, HttpServletResponse.SC_NOT_FOUND, e.getMessage());
         } catch (ServletContextException | DatabaseException | StorageException | IllegalStateException e) {
+            log.error("An error occurred while processing the request", e);
             handleException(req, resp, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
         } catch (Exception e) {
-            handleException(req, resp, HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-                    "Unknown server error: " + e.getMessage());
+            log.error("An unknown error occurred while processing the request", e);
+            handleException(req, resp, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "unknown server error");
         }
     }
 
